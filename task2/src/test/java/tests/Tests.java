511e676e4e284da24d9aef67ac904567c7a43aee
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-10-20 17:02:17 -0600

    Fixed login page is open

--- task2/src/test/java/tests/Tests.java
+++ task2/src/test/java/tests/Tests.java
@@ -3,15 +3,21 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class Tests {
 
     WebDriver driver;
+    WebDriverWait wait;
     String url = "https://store.steampowered.com/";
 
     @BeforeMethod
@@ -19,27 +25,39 @@ public class Tests {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @Test
     public void loginTest() {
+        // Navigate to main page
         driver.get(url);
 
         // Main page is displayed
+        // TODO: I DON'T LIKE THIS
         String actualTitle = driver.getTitle();
         String expectedTitle = "Bienvenidos a Steam";
         Assert.assertEquals(actualTitle, expectedTitle, "Landed on wrong main page!");
 
+        // Click login link
+        WebElement loginLink = driver.findElement(By.cssSelector("a.global_action_link"));
+        loginLink.click();
+
         // Login page is open
-        driver.findElement(By.cssSelector("a.global_action_link")).click();
-        actualTitle = driver.getTitle();
-        expectedTitle = "Iniciar sesión";
-        Assert.assertEquals(actualTitle, expectedTitle, "Landed on wrong login page!");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,'SubmitButton')]")));
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@class,'SubmitButton')]"));
+        Assert.assertTrue(loginButton.isDisplayed(), "Login page is not opened!");
+
+        // Input random strings as credentials
+        // Click sign in button
+
+        // Loading element is displayed
+        // Error text is displayed (after loading element disappearing)
     }
 
     @AfterMethod
     public void teardown() {
         driver.quit();
     }
-
 }
