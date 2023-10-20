f5e5d98927e2fe515e98395cab8cc483740ddcc0
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-10-20 15:44:30 -0600

    Assert main page is displayed

--- task2/src/test/java/tests/Tests.java
+++ task2/src/test/java/tests/Tests.java
@@ -3,6 +3,8 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,11 +17,22 @@ public class Tests {
     public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
     public void loginTest() {
         driver.get(url);
+
+        // Main page is displayed
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Bienvenidos a Steam";
+        Assert.assertEquals(actualTitle, expectedTitle, "Landed on wrong page!");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
     }
 
 }
