4be7d82ed143c509f3844fe885b702006f53c534
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-10-21 17:51:29 -0600

    Implemented BaseElement and Singleton

--- task2/src/test/java/test/Tests.java
+++ task2/src/test/java/test/Tests.java
@@ -1,78 +1,71 @@
 package test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import util.BaseElement;
+import util.Browser;
 import util.RandomUtils;
 
 import java.time.Duration;
 
 public class Tests {
-
-    WebDriver driver;
     WebDriverWait wait;
     String url = "https://store.steampowered.com/";
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        // Navigate to main page
+        Browser.getDriver().get(url);
+        Browser.getDriver().manage().window().maximize();
 
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait = new WebDriverWait(Browser.getDriver(), Duration.ofSeconds(5));
     }
 
     @Test
     public void loginTest() {
-        // Navigate to main page
-        driver.get(url);
-
         // Main page is displayed
-        WebElement homePageContent = driver.findElement(By.cssSelector(".home_page_content"));
+        BaseElement homePageContent = new BaseElement(By.cssSelector(".home_page_content"));
         Assert.assertTrue(homePageContent.isDisplayed(), "Main page is not opened!");
 
         // Click login link
-        WebElement loginLink = driver.findElement(By.cssSelector("a.global_action_link"));
+        BaseElement loginLink = new BaseElement(By.cssSelector("a.global_action_link"));
         loginLink.click();
 
         // Login page is open
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@class,'SubmitButton')]")));
-        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@class,'SubmitButton')]"));
+        BaseElement loginButton = new BaseElement(By.xpath("//button[contains(@class,'SubmitButton')]"));
         Assert.assertTrue(loginButton.isDisplayed(), "Login page is not opened!");
 
         // Input random strings as credentials
         String randomUsername = RandomUtils.generateRandomString(8);
-        WebElement usernameField = driver.findElement(By.xpath("//input[@type='text']"));
+        BaseElement usernameField = new BaseElement(By.xpath("//input[@type='text']"));
         usernameField.sendKeys(randomUsername);
 
         String randomPassword = RandomUtils.generateRandomString(8);
-        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        BaseElement passwordField = new BaseElement(By.xpath("//input[@type='password']"));
         passwordField.sendKeys(randomPassword);
 
         // Click sign in button
-        WebElement signInButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        BaseElement signInButton = new BaseElement(By.xpath("//button[@type='submit']"));
         signInButton.click();
 
         // Loading element is displayed
-        WebElement spinner = driver.findElement(By.xpath("//*[contains(@class,'LoadingSpinner')]"));
+        BaseElement spinner = new BaseElement(By.xpath("//*[contains(@class,'LoadingSpinner')]"));
         Assert.assertTrue(spinner.isDisplayed(), "Loading element is not displayed!");
 
         // Error text is displayed (after loading element disappearing)
-        wait.until(ExpectedConditions.invisibilityOf(spinner));
-        WebElement errorText = driver.findElement(By.xpath("//*[contains(@class,'FormError')]"));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[contains(@class,'LoadingSpinner')]")));
+        BaseElement errorText = new BaseElement(By.xpath("//*[contains(@class,'FormError')]"));
         Assert.assertTrue(errorText.isDisplayed(), "Error text is not displayed!");
     }
 
     @AfterMethod
     public void teardown() {
-        driver.quit();
+        Browser.quitDriver();
     }
 }
